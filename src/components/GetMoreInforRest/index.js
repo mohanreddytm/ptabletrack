65d7fd77e0348d3f888ed27911639abb693813d3
mohanreddytm <tamminenimohanreddy1@gmail.com>
2025-07-10 16:58:57 +0530

    added the menu cat, tables to get the details of the restaurent

--- src/components/GetMoreInforRest/index.js
+++ src/components/GetMoreInforRest/index.js
@@ -4,7 +4,9 @@ import { useState } from 'react';
 
 import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
 
-import { MdDelete } from "react-icons/md";
+import { IoMdAdd } from "react-icons/io";
+
+import { MdDeleteForever as MdDelete } from "react-icons/md";
 
 import './index.css'
 
@@ -14,11 +16,13 @@ const GetMoreInforRest = () => {
 
     const [tables, setTables] = useState([]);
 
+    const [menuCategories, setMenuCategories] = useState([]);
+
 
 
     const [areaOneInput, setAreaOneInput] = useState('');
     const [currentStep, setCurrentStep] = useState("area");
-
+    const [menuCategoryInput, setMenuCategoryInput] = useState('');
     const [tableStatus, setTableStatus] = useState('active');
     const [tableName, setTableName] = useState('');
     const [seatingCapacity, setSeatingCapacity] = useState('');
@@ -58,7 +62,53 @@ const GetMoreInforRest = () => {
 
     console.log("Current Input:", areaOneInput);
 
-    const RestaurantAreasOne = () => {
+    const onSubmitOfTable = (e) => {
+        e.preventDefault();
+        console.log("entered")
+        console.log(tableName, seatingCapacity, selectedArea);
+
+        if(tableName.trim() !== '' && seatingCapacity.trim() !== '' && selectedArea.trim() !== ''){
+            
+            const newtable = {
+                "table_name": tableName,
+                "seating_capacity": seatingCapacity,
+                "area": selectedArea,
+                "status": tableStatus
+            }
+            setTables([...tables, newtable]);
+            setTableName('');
+            setSeatingCapacity('');
+            setSelectedArea('');
+            setTableStatus("active");
+        }
+
+    }
+
+
+    const onClickTableDeleteIcon = (tableName) => {
+        const updatedTables = tables.filter(table => table.table_name !== tableName);
+        setTables(updatedTables);
+    }
+
+
+
+    const onSubmitMenuCategory = (e) => {
+        e.preventDefault();
+        if(menuCategoryInput.trim() !== ''){
+            setMenuCategories([...menuCategories, menuCategoryInput])
+            setMenuCategoryInput('');
+        }else{
+            setMenuCategoryInput('');
+        }
+    } 
+
+    const onClickSaveAndNextTable = () => {
+        if(tables.length > 0) {
+            setCurrentStep("menu");
+        }
+    }
+
+        const RestaurantAreasOne = () => {
         return (
             <form onSubmit={onSubmitAddArea} className='gm-use-cont'>
                 <h1 className='gm-use-title'>Set Up Your Restaurant Areas</h1>
@@ -78,7 +128,7 @@ const GetMoreInforRest = () => {
                     <button type='submit' className='gm-add-area-btn'>Add Area</button>
                     <button type='button' onClick={onClickSaveNxtAreas} className={`gm-next-btn ${areas.length > 0 && "gm-next-done-btn"}`}>Save & Next</button>
                 </div>
-                <p className='gm-skip'>Skip for now <FaArrowRightLong className='gm-skip-icon' /> </p>
+                <p className='gm-skip' onClick={() => setCurrentStep("table")}>Skip for now <FaArrowRightLong className='gm-skip-icon' /> </p>
                 <div>
                     <h1 className='gm-use-bottom-head'>Your Restaurant Areas</h1>
                     {areas.length > 0 ? (
@@ -98,39 +148,50 @@ const GetMoreInforRest = () => {
 
     const AddingTables = () => {
         return (
-            <form className='gm-use-cont gm-use-table-cont'>
+            <form onSubmit={onSubmitOfTable} className='gm-use-cont gm-use-table-cont'>
                 <h1 className='gm-use-title'>Add Tables to Your Restaurant Areas</h1>
                 <p className='gm-use-description'>To add tables to your restaurant areas, please provide the details of the tables you want to add.</p>
+
                 <h1 className='gm-use-add-area-one'>Add Table</h1>
-                <select className='gm-input-area'>
-                    <option value=''>Select Area</option>
+                <div className='gm-use-add-area-cont-mini'><h1 className='gm-use-add-area-one'>Select Area</h1> <button type='button' onClick={() => setCurrentStep("area")} className='gm-table-add-area-one-b'><IoMdAdd /> Add</button></div>
+                <select onChange={(e) => setSelectedArea(e.target.value)} className='gm-input-area' required>
+                    <option value={selectedArea}>Select Area</option>
                     {areas.map((area, index) => (
                         <option key={index} value={area}>{area}</option>
                     ))}
                 </select>
                 <h1 className='gm-use-add-area-one'>Table Name</h1>
-                <input className='gm-input-area' type='text' placeholder='Ex: T01' required />
+                <input value={tableName} onChange={(e) => setTableName(e.target.value)} className='gm-input-area' type='text' placeholder='Ex: T01' required />
                 <h1 className='gm-use-add-area-one'>Seating Capacity</h1>
-                <input className='gm-input-area' type='number' placeholder='Number of Seats (Ex: 4)' required />
+                <input value={seatingCapacity} onChange={(e) => setSeatingCapacity(e.target.value)} className='gm-input-area' type='number' placeholder='Number of Seats (Ex: 4)' required />
                 <h1 className='gm-use-add-area-one'>Status</h1>
                 <div>
-                    <button type='button' className='gm-active-btn'>Active</button>
-                    <button type='button' className='gm-inactive-btn'>Inactive</button>
+                    <button type='button' onClick={() => setTableStatus("active")} className={`gm-active-btn ${tableStatus === "active" && "gm-active-true"}`}>Active</button>
+                    <button type='button' onClick={() => setTableStatus("inactive")} className={`gm-inactive-btn ${tableStatus === "inactive" && "gm-inactive-true"}`}>Inactive</button>
                 </div>
                 <div className='gm-add-area-btn-cont'>
                     <button type='submit' className='gm-add-area-btn'>Add Table</button>
-                    <button type='button'  className={`gm-next-btn ${areas.length > 0 && "gm-next-done-btn"}`}>Save & Next</button>
+                    <button type='button' onClick={onClickSaveAndNextTable} className={`gm-next-btn ${tables.length > 0 && "gm-next-done-btn"}`}>Save & Next</button>
                 </div>
                 <div className='gm-skip-cont'>
                     <p className='gm-back' onClick={onClickBackFromTable}><FaArrowLeftLong className='gm-skip-icon' /> Go Back</p>
-                    <p className='gm-skip'>Skip for now <FaArrowRightLong className='gm-skip-icon' /> </p>
+                    <p className='gm-skip' onClick={() => setCurrentStep("menu")}>Skip for now <FaArrowRightLong className='gm-skip-icon' /> </p>
                 </div>
-                                <div>
+                <div>
                     <h1 className='gm-use-bottom-head'>Your Restaurant Tables</h1>
                     {tables.length > 0 ? (
                         <ul className='gm-use-bottom-ul'>
                             {tables.map((table, index) => (
-                                <li key={index} className='gm-use-bottom-p'>{table.name} <MdDelete onClick={() => onClickDeleteIcon(table.name)} className='gm-delete-icon' /></li>
+                                <li key={index} className='gm-use-bottom-p gm-use-bottom-p-tables'>
+                                    <div className='gm-table-details'>
+                                        <h1 className='gm-tables-lines gm-table-main-p'>Area: <span className='gm-table-area-sp'>{table.area}</span></h1>
+                                        <h1 className='gm-tables-lines'>Table Name: <span className='gm-table-name-sp'>{table.table_name}</span></h1>
+                                        <h1 className='gm-tables-lines'>Seating Capacity: <span className='gm-table-seating-sp'>{table.seating_capacity}</span></h1>
+                                        
+                                        <h1 className='gm-tables-lines'>Status: <span className={`${table.status === "active"  ? "gm-table-active-sp" :"gm-table-inactive-sp" }`}>{table.status}</span></h1>
+
+                                    </div>
+                                    <MdDelete onClick={() => onClickTableDeleteIcon(table.table_name)} className='gm-delete-icon gm-delete-table-p' /></li>
                             ))}
                         </ul>
                     ) : (
@@ -141,12 +202,49 @@ const GetMoreInforRest = () => {
         )
     }
 
+    const AddMenu = () => {
+        return (
+            <form onSubmit={onSubmitMenuCategory} className='gm-use-cont gm-use-cont-menu'>
+                <h1 className='gm-use-title'>Add a Menu Category</h1>
+                <p className='gm-use-description'>
+                Enter a category name for your restaurant menu — like Breakfast, Lunch, Drinks, Ice Creams, Desserts, or Fast Food.
+                </p>
+                <h1 className='gm-use-add-area-one'>Add Menu Category</h1>
+                <input onChange={(e) => setMenuCategoryInput(e.target.value)} value={menuCategoryInput} className='gm-input-area' type='text' placeholder='Ex: Breakfast' required />
+                <div className='gm-add-area-btn-cont'>
+                    <button type='submit' className='gm-add-area-btn'>Add Menu</button>
+                    <button type='button' onClick={() => setCurrentStep("area")} className={`gm-next-btn ${menuCategories.length > 0 && "gm-next-btn-disabled"}`}>Save & Next</button>
+                </div>
+                <div className='gm-skip-cont'>
+                    <p className='gm-back' onClick={() => setCurrentStep("table")}><FaArrowLeftLong className='gm-skip-icon' /> Go Back</p>
+                    <p className='gm-skip'>Skip for now <FaArrowRightLong className='gm-skip-icon' /> </p>
+                </div>
+                <div>
+                    <h1 className='gm-use-bottom-head'>Your Menu Categories</h1>
+                    {
+                        menuCategories.length > 0 ? (
+                            <ul className='gm-use-bottom-ul'>
+                                {menuCategories.map((category, index) => (
+                                    <li key={index} className='gm-use-bottom-p'>{category} <MdDelete onClick={() => setMenuCategories(menuCategories.filter(cat => cat !== category))} className='gm-delete-icon' /></li>
+                                ))}
+                            </ul>
+                        ) : 
+                        <p className='gm-use-bottom-p'>You have not added any menu categories yet.</p>
+                    }
+                </div>
+
+            </form>
+        )
+    }
+
     const MainOne = () => {
         switch (currentStep) {
             case "area":
                 return RestaurantAreasOne()
             case "table":
                 return AddingTables()
+            case "menu":
+                return AddMenu()
             default:
                 return RestaurantAreasOne()
         }
